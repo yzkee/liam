702bc4cc439d1964c6d981cc15afe39723ef5cc9
MH4GF <h.miyagi.cnw@gmail.com>
2025-06-02 17:55:44 +0900

    🔥 remove unused documentation and migration components from the project structure

--- frontend/apps/app/components/KnowledgeSuggestionDetailPage/index.ts
+++
@@ -1,1 +0,0 @@
-export { KnowledgeSuggestionDetailPage } from './KnowledgeSuggestionDetailPage'
--- frontend/apps/app/components/MigrationDetailPage/components/ReviewFeedbackList/components/ReviewFeedbackComments/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './ReviewFeedbackComments'
-export * from './CommentForm/CommentForm'
-export * from './CommentList/CommentList'
-export * from './CommentsModal/CommentsModal'

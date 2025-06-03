b63b3a6fadc7e57e841b76ab4bd6d96dff1e92a8
Tomoya Kashifuku <tnyo43@gmail.com>
2025-06-03 21:43:02 +0900

    🐛 set dialog title and description to avoid error
    @radix-ui/react-dialog requires title and description.

--- frontend/packages/erd-core/src/features/erd/components/ERDRenderer/CommandPalette/CommandPalette.tsx
+++ frontend/packages/erd-core/src/features/erd/components/ERDRenderer/CommandPalette/CommandPalette.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { DialogDescription, DialogTitle } from '@radix-ui/react-dialog'
 import { Command } from 'cmdk'
 import { type FC, useEffect, useState } from 'react'
 import styles from './CommandPalette.module.css'
@@ -26,6 +27,11 @@ export const CommandPalette: FC = () => {
       onOpenChange={setOpen}
       contentClassName={styles.content}
     >
+      <DialogTitle hidden>Command Palette</DialogTitle>
+      <DialogDescription hidden>
+        A search-based interface that allows quick access to various commands
+        and features within the application.
+      </DialogDescription>
       Coming Soon...
     </Command.Dialog>
   )

fefb039cf0bebecd7ea73eea906a325679a5393f
Tomoya Kashifuku <tnyo43@gmail.com>
2025-06-22 15:20:54 +0900

    ✅ add e2e test case for CommandPalette component

---
+++ frontend/internal-packages/e2e/tests/e2e/commandPalette.test.ts
@@ -0,0 +1,25 @@
+import { expect, test } from '@playwright/test'
+
+test.beforeEach(async ({ page, isMobile }) => {
+  if (isMobile) {
+    test.skip()
+  }
+
+  await page.goto('/')
+})
+
+test('go to table in ERD by CommandPalette', async ({ page }) => {
+  // Open dialog
+  await page.keyboard.press('Control+k')
+  const commandPaletteDialog = await page.getByRole('dialog', {
+    name: 'Command Palette',
+  })
+  await expect(commandPaletteDialog).toBeVisible()
+
+  // Select "user_roles" option
+  await page.keyboard.type('user_roles')
+  await page.keyboard.press('Enter')
+
+  // Go to ERD
+  await expect(page).toHaveURL(/.*active=user_roles/)
+})

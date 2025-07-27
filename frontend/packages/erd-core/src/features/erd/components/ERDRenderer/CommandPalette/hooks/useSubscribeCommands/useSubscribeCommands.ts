f93dd071d7fa7f6718a2997d3bb8bc0f28c062b6
Tomoya Kashifuku <tnyo43@gmail.com>
2025-07-27 10:30:00 +0900

    ✨ support Zoom to Fit command

--- frontend/packages/erd-core/src/features/erd/components/ERDRenderer/CommandPalette/hooks/useSubscribeCommands/useSubscribeCommands.ts
+++ frontend/packages/erd-core/src/features/erd/components/ERDRenderer/CommandPalette/hooks/useSubscribeCommands/useSubscribeCommands.ts
@@ -1,7 +1,9 @@
 import { useSubscribeCopyLinkCommand } from './useSubscribeCopyLinkCommand'
 import { useSubscribeShowModeCommand } from './useSubscribeSwitchShowMode'
+import { useSubscribeZoomToFitCommand } from './useSubscribeZoomToFitCommand'
 
 export const useSubscribeCommands = () => {
   useSubscribeCopyLinkCommand()
   useSubscribeShowModeCommand()
+  useSubscribeZoomToFitCommand()
 }

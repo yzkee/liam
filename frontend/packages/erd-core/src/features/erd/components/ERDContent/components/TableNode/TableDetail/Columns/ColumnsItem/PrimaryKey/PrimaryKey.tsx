1bb7ad3c705b8374aa4cc3fdacfb6f84ec323d1c
junkisai <jonki.beam@gmail.com>
2025-08-04 17:06:25 +0900

    ✨ add diff highlighting support for column primary key changes

---
+++ frontend/packages/erd-core/src/features/erd/components/ERDContent/components/TableNode/TableDetail/Columns/ColumnsItem/PrimaryKey/PrimaryKey.tsx
@@ -0,0 +1,36 @@
+import { GridTableItem, GridTableRow, KeyRound } from '@liam-hq/ui'
+import { type FC, useMemo } from 'react'
+import { useDiffStyle } from '@/features/diff/hooks/useDiffStyle'
+import { useSchemaOrThrow, useUserEditingOrThrow } from '@/stores'
+import { getChangeStatus } from './getChangeStatus'
+import styles from './PrimaryKey.module.css'
+
+type Props = {
+  tableId: string
+  columnName: string
+}
+
+export const PrimaryKey: FC<Props> = ({ tableId, columnName }) => {
+  const { diffItems } = useSchemaOrThrow()
+  const { showDiff } = useUserEditingOrThrow()
+
+  const changeStatus = useMemo(() => {
+    if (!showDiff) return undefined
+    return getChangeStatus({
+      tableId,
+      columnId: columnName,
+      diffItems: diffItems ?? [],
+    })
+  }, [showDiff, tableId, diffItems, columnName])
+
+  const diffStyle = useDiffStyle(showDiff, changeStatus)
+
+  return (
+    <GridTableItem className={diffStyle}>
+      <GridTableRow>
+        <KeyRound className={styles.primaryKeyIcon} />
+        Primary Key
+      </GridTableRow>
+    </GridTableItem>
+  )
+}

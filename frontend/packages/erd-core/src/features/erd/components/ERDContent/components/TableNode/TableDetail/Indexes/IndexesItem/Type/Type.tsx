468ef0f34c3713c4629236fe8955ed83adff3fbc
junkisai <jonki.beam@gmail.com>
2025-08-04 18:01:19 +0900

    ✨ Add diff visualization for index type

---
+++ frontend/packages/erd-core/src/features/erd/components/ERDContent/components/TableNode/TableDetail/Indexes/IndexesItem/Type/Type.tsx
@@ -0,0 +1,35 @@
+import type { Index } from '@liam-hq/db-structure'
+import { GridTableDd, GridTableDt, GridTableItem } from '@liam-hq/ui'
+import clsx from 'clsx'
+import { type FC, useMemo } from 'react'
+import { useDiffStyle } from '@/features/diff/hooks/useDiffStyle'
+import { useSchemaOrThrow, useUserEditingOrThrow } from '@/stores'
+import { getChangeStatus } from './getChangeStatus'
+
+type Props = {
+  tableId: string
+  index: Index
+}
+
+export const Type: FC<Props> = ({ tableId, index }) => {
+  const { diffItems } = useSchemaOrThrow()
+  const { showDiff } = useUserEditingOrThrow()
+
+  const changeStatus = useMemo(() => {
+    if (!showDiff) return undefined
+    return getChangeStatus({
+      tableId,
+      diffItems: diffItems ?? [],
+      indexId: index.name,
+    })
+  }, [showDiff, tableId, diffItems, index.name])
+
+  const diffStyle = useDiffStyle(showDiff, changeStatus)
+
+  return (
+    <GridTableItem className={clsx(diffStyle)}>
+      <GridTableDt>Type</GridTableDt>
+      <GridTableDd>{index.type}</GridTableDd>
+    </GridTableItem>
+  )
+}

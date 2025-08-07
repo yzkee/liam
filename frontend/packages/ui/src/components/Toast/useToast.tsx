2d34d10fd37ef41ede9af5bfd136be72ed86adab
Tomoya Kashifuku <tnyo43@gmail.com>
2025-08-08 08:18:36 +0900

    ✨ make Toast position switchable

--- frontend/packages/ui/src/components/Toast/useToast.tsx
+++ frontend/packages/ui/src/components/Toast/useToast.tsx
@@ -1,7 +1,13 @@
 import { useContext } from 'react'
 import { ToastContext } from './Toast'
+import type { ToastPosition } from './types'
 
-export const useToast = () => {
-  const { headerToast } = useContext(ToastContext)
-  return headerToast
+export const useToast = (position: ToastPosition = 'header') => {
+  const { headerToast, commandPaletteToast } = useContext(ToastContext)
+  switch (position) {
+    case 'header':
+      return headerToast
+    case 'command-palette':
+      return commandPaletteToast
+  }
 }

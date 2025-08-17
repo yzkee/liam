78fae702ba928dcd34d94158b8a3f6ae9533e53e
Tomoya Kashifuku <tnyo43@gmail.com>
2025-08-17 11:38:11 +0900

    ✨ use inputMode in the CommandPaletteContent

--- frontend/packages/erd-core/src/features/erd/components/ERDRenderer/CommandPalette/CommandPaletteSearchInput/CommandPaletteSearchInput.tsx
+++ frontend/packages/erd-core/src/features/erd/components/ERDRenderer/CommandPalette/CommandPaletteSearchInput/CommandPaletteSearchInput.tsx
@@ -1,15 +1,27 @@
 import { Search } from '@liam-hq/ui'
 import { Command } from 'cmdk'
 import type { ComponentProps, FC } from 'react'
+import type { CommandPaletteInputMode } from '../types'
 import styles from './CommandPaletteSearchInput.module.css'
 
-type Props = ComponentProps<typeof Command.Input>
+type Props = ComponentProps<typeof Command.Input> & {
+  mode: CommandPaletteInputMode
+  setMode: (mode: CommandPaletteInputMode) => void
+}
 
-export const CommandPaletteSearchInput: FC<Props> = (props) => {
+export const CommandPaletteSearchInput: FC<Props> = ({
+  mode,
+  setMode,
+  ...inputProps
+}) => {
   return (
     <div className={styles.container}>
       <Search className={styles.searchIcon} />
-      <Command.Input {...props} className={styles.input} placeholder="Search" />
+      <Command.Input
+        {...inputProps}
+        className={styles.input}
+        placeholder="Search"
+      />
     </div>
   )
 }

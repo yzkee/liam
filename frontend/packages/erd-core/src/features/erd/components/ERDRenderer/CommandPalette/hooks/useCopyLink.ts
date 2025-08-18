3a5e003b2bf7dcc748fcb606eeac7d74abdb5e90
Tomoya Kashifuku <tnyo43@gmail.com>
2025-08-19 08:54:09 +0900

    💄 show Toast feedback with useCopyLink

--- frontend/packages/erd-core/src/features/erd/components/ERDRenderer/CommandPalette/hooks/useCopyLink.ts
+++ frontend/packages/erd-core/src/features/erd/components/ERDRenderer/CommandPalette/hooks/useCopyLink.ts
@@ -2,7 +2,13 @@ import { useCopy } from '@liam-hq/ui/hooks'
 import { useCallback } from 'react'
 
 export const useCopyLink = () => {
-  const { copy } = useCopy()
+  const { copy } = useCopy({
+    toast: {
+      success: 'Link copied!',
+      error: 'URL copy failed',
+      position: 'command-palette',
+    },
+  })
 
   const copyLink = useCallback(() => {
     const url = window.location.href

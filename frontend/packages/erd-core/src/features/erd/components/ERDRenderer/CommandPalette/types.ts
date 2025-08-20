803721c91fe24a91f5ed1428d0b1c3bed7b15db8
Tomoya Kashifuku <tnyo43@gmail.com>
2025-08-21 08:13:47 +0900

    ✨ add suggestion type

--- frontend/packages/erd-core/src/features/erd/components/ERDRenderer/CommandPalette/types.ts
+++ frontend/packages/erd-core/src/features/erd/components/ERDRenderer/CommandPalette/types.ts
@@ -1,3 +1,7 @@
 export type CommandPaletteInputMode = { type: 'default' } | { type: 'command' }
 // upcoming input mode
 // | { type: 'column'; tableName: string }
+
+export type CommandPaletteSuggestion =
+  | { type: 'table'; name: string }
+  | { type: 'command'; name: string }

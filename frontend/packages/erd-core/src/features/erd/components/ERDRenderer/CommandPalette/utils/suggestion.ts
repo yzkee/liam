e424a83102253cbcd0a4907a3c584dc30967078c
Tomoya Kashifuku <tnyo43@gmail.com>
2025-08-21 20:40:00 +0900

    ♻️ declare separator character

--- frontend/packages/erd-core/src/features/erd/components/ERDRenderer/CommandPalette/utils/suggestion.ts
+++ frontend/packages/erd-core/src/features/erd/components/ERDRenderer/CommandPalette/utils/suggestion.ts
@@ -1,12 +1,14 @@
 import type { CommandPaletteSuggestion } from '../types'
 
+const SEPARATOR = '|'
+
 export const getSuggestionText = (suggestion: CommandPaletteSuggestion) =>
-  `${suggestion.type}|${suggestion.name}`
+  `${suggestion.type}${SEPARATOR}${suggestion.name}`
 
 export const textToSuggestion = (
   text: string,
 ): CommandPaletteSuggestion | null => {
-  const words = text.split('|')
+  const words = text.split(SEPARATOR)
 
   const [suggestionType, name] = words
   if (!suggestionType || !name) return null

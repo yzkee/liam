d41d1d63f9c359c9446a8a2fbfd6c710a3cb359a
Tomoya Kashifuku <tnyo43@gmail.com>
2025-08-31 14:08:36 +0900

    ✨ support video previews

--- frontend/packages/erd-core/src/features/erd/components/ERDRenderer/CommandPalette/CommandPalettePreview/CommandPreview.tsx
+++ frontend/packages/erd-core/src/features/erd/components/ERDRenderer/CommandPalette/CommandPalettePreview/CommandPreview.tsx
@@ -1,11 +1,20 @@
 import type { FC } from 'react'
+import copyLink from './assets/copy-link.mp4'
 import showAllFields from './assets/show-all-fields.png'
 import showKeyOnly from './assets/show-key-only.png'
 import showTableName from './assets/show-table-name.png'
+import tidyUp from './assets/tidy-up.mp4'
+import zoomToFit from './assets/zoom-to-fit.mp4'
 import styles from './CommandPalettePreview.module.css'
 
 type Props = {
   commandName: string
+}
+
+const COMMAND_VIDEO_SOURCE: Record<string, string> = {
+  'copy link': copyLink,
+  'Zoom to Fit': zoomToFit,
+  'Tidy Up': tidyUp,
 }
 
 const COMMAND_IMAGE_SOURCE: Record<string, string> = {
@@ -17,6 +26,11 @@ const COMMAND_IMAGE_SOURCE: Record<string, string> = {
 export const CommandPreview: FC<Props> = ({ commandName }) => {
   return (
     <div className={styles.container}>
+      {COMMAND_VIDEO_SOURCE[commandName] && (
+        <video muted autoPlay className={styles.video} key={commandName}>
+          <source src={COMMAND_VIDEO_SOURCE[commandName]} type="video/mp4" />
+        </video>
+      )}
       {COMMAND_IMAGE_SOURCE[commandName] && (
         <img
           src={COMMAND_IMAGE_SOURCE[commandName]}

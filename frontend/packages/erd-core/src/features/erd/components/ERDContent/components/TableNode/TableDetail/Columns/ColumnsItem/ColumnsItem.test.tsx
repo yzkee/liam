d6df478865b242c4528ee398dfd2b92660397f0c
Tomoya Kashifuku <tnyo43@gmail.com>
2025-09-15 08:59:45 +0900

    ✅ update test

--- frontend/packages/erd-core/src/features/erd/components/ERDContent/components/TableNode/TableDetail/Columns/ColumnsItem/ColumnsItem.test.tsx
+++ frontend/packages/erd-core/src/features/erd/components/ERDContent/components/TableNode/TableDetail/Columns/ColumnsItem/ColumnsItem.test.tsx
@@ -18,7 +18,7 @@ const wrapper: FC<PropsWithChildren> = ({ children }) => (
 )
 
 describe('id', () => {
-  it('renders column name as a heading', () => {
+  it('renders column name as a inked heading', () => {
     render(
       <ColumnsItem
         tableId="users"
@@ -29,8 +29,12 @@ describe('id', () => {
     )
 
     expect(
-      screen.getByRole('heading', { name: 'id', level: 3 }),
+      screen.getByRole('heading', { name: 'id #', level: 3 }),
     ).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: 'id #' })).toHaveAttribute(
+      'href',
+      '#users__columns__id',
+    )
   })
 })
 

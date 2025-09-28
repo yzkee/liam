5d1e60befe92a9c5063df80394107fb6f153135b
Tomoya Kashifuku <tnyo43@gmail.com>
2025-09-28 18:25:16 +0900

    ✨ add column Input Mode

--- frontend/packages/erd-core/src/features/erd/components/ERDRenderer/CommandPalette/types.ts
+++ frontend/packages/erd-core/src/features/erd/components/ERDRenderer/CommandPalette/types.ts
@@ -1,6 +1,7 @@
-export type CommandPaletteInputMode = { type: 'default' } | { type: 'command' }
-// upcoming input mode
-// | { type: 'column'; tableName: string }
+export type CommandPaletteInputMode =
+  | { type: 'default' }
+  | { type: 'command' }
+  | { type: 'column'; tableName: string }
 
 export type CommandPaletteSuggestion =
   | { type: 'table'; name: string }
